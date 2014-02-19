d214788a708b2b02def92d308918572908bfe334
Auvinet <guillaume.auvinet@gmail.com>
2014-02-19 12:47:50 +0100

    Add Leaflet dependencies

--- myApp.php
+++ myApp.php
@@ -1,5 +1,11 @@
 <html>
 <head>
+	<title>WhoSupportMyTeam</title>
+	<meta charset="utf-8" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	 <link rel="stylesheet" href="css/leaflet.css" />
 </head>
 <body>
 <?php
@@ -14,8 +20,14 @@ $accesstokensecret = $_SESSION['oauth_token_secret'];
 $twitter = new TwitterOAuth($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
 $credentials = $twitter->getAccessToken($_GET["oauth_verifier"]);
 $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=%fourcade');
-print_r($tweets);
+//print_r($tweets);
 ?>
-
+<h1>WhoSupportMyTeam</h1>
+<h2>Geolocalize Tweet about your favorite team</h2>
+<div id="map" style="width: 600px; height: 400px"></div>
+<script src="js/leaflet.js"
+var map = L.map('map').setView([51.505, -0.09], 13);
+></script>
 </body>
+
 </html>
